04491b52c029149e6bfe5cbd57263f90c1f4f814
Abdo1629 <boody1629@gmail.com>
2025-07-30 17:38:37 +0300

    edit firebase file

--- src/app/lib/firebase-admin.ts
+++ src/app/lib/firebase-admin.ts
@@ -1,11 +1,12 @@
 // lib/firebase-admin.ts
 import { initializeApp, cert, getApps, getApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
+import type { ServiceAccount } from "firebase-admin";
 import serviceAccount from "../../../../serviceAccountKey.json";
 
 const app = !getApps().length
   ? initializeApp({
-      credential: cert(serviceAccount as any),
+      credential: cert(serviceAccount as ServiceAccount),
     })
   : getApp();
 
